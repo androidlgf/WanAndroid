30672d67bf275dabdfe48885c8a4b26aa5469474
androidlgf <android_lgf@163.com>
2019-01-28 19:01:53 +0800

    Indicator底部对齐

--- wanandroid/lib/widget/indicator_widget.dart
+++ wanandroid/lib/widget/indicator_widget.dart
@@ -43,10 +43,10 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
     return Stack(children: [
       CarouselSlider(
         viewportFraction: 1.0,
-        items: images.map((i) {
+        items: images.map((imageUrl) {
           return Builder(builder: (BuildContext context) {
             return Image.network(
-              i,
+              imageUrl,
               fit: BoxFit.cover,
               width: 1000.0,
             );
@@ -64,19 +64,22 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
           top: 0.0,
           left: 0.0,
           right: 0.0,
+          bottom: 0.0,
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.center,
             verticalDirection: VerticalDirection.down,
             children: map<Widget>(images, (index, url) {
               return Container(
                 width: 8.0,
                 height: 8.0,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+//                alignment: Alignment.bottomCenter,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
                 decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: _current == index
-                        ? Color.fromRGBO(0, 0, 0, 0.9)
-                        : Color.fromRGBO(0, 0, 0, 0.4)),
+                        ? Color.fromARGB(255, 255, 62, 150)
+                        : Color.fromARGB(255, 255, 130, 171)),
               );
             }),
           ))
