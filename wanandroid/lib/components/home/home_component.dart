cf6e5e449f2369e6e1ec56b2f00caa661ebc4bb3
androidlgf <android_lgf@163.com>
2019-03-19 17:53:17 +0800

    首页添加Drawer

--- wanandroid/lib/components/home/home_component.dart
+++ wanandroid/lib/components/home/home_component.dart
@@ -15,6 +15,69 @@ class _HomeComponentState extends State<HomeComponent> {
       appBar: AppBar(
         title: Text('Home'),
       ),
+      drawer: Drawer(
+        child: HomeBuilder.homeDrawer(),
+      ),
+      body: Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
+class HomeBuilder {
+  static Widget homeDrawer() {
+    return new ListView(padding: const EdgeInsets.only(), children: <Widget>[
+      _drawerHeader(),
+      new ClipRect(
+        child: new ListTile(
+          leading: new CircleAvatar(child: new Text("A")),
+          title: new Text('Drawer item A'),
+          onTap: () => {},
+        ),
+      ),
+      new ListTile(
+        leading: new CircleAvatar(child: new Text("B")),
+        title: new Text('Drawer item B'),
+        subtitle: new Text("Drawer item B subtitle"),
+        onTap: () => {},
+      ),
+      new AboutListTile(
+        icon: new CircleAvatar(child: new Text("Ab")),
+        child: new Text("About"),
+        applicationName: "Test",
+        applicationVersion: "1.0",
+        applicationIcon: new Image.asset(
+          "images/ymj_jiayou.gif",
+          width: 64.0,
+          height: 64.0,
+        ),
+        applicationLegalese: "applicationLegalese",
+        aboutBoxChildren: <Widget>[
+          new Text("BoxChildren"),
+          new Text("box child 2")
+        ],
+      ),
+    ]);
+  }
+
+  static Widget _drawerHeader() {
+    return new UserAccountsDrawerHeader(
+//      margin: EdgeInsets.zero,
+      accountName: new Text(
+        "SuperLuo",
+      ),
+      accountEmail: new Text(
+        "super_luo@163.com",
+      ),
+      currentAccountPicture: new CircleAvatar(
+        backgroundImage: new AssetImage("images/ymj_jiayou.gif"),
+      ),
+      onDetailsPressed: () {},
+      otherAccountsPictures: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new AssetImage("images/ymj_shuijiao.gif"),
+        ),
+      ],
     );
   }
 }
