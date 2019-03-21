8dc1c059c5a5791fcc2e3697dd151fa9e815a2ef
androidlgf <android_lgf@163.com>
2019-03-21 16:32:47 +0800

    add async_future_builder

---
+++ wanandroid/lib/widget/async_future_builder.dart
@@ -0,0 +1,70 @@
+import 'dart:async' show Future;
+import 'package:flutter/material.dart';
+//重复刷新解决
+// 1.AsyncMemoizer asyncMemoizer = AsyncMemoizer(); memoizer.runOnce(() async{}
+// 2.initState()/
+class CommonFutureBuilder<T> extends StatefulWidget {
+  CommonFutureBuilder(
+      {@required this.contentWidget,
+      this.future,
+      this.connectionSucceed,
+      this.connectionError});
+
+  final Widget contentWidget;
+  final Future<T> future;
+  final Function(Object response) connectionSucceed;
+  final Function(Object errorInfo) connectionError;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CommonFutureBuilderState();
+  }
+}
+
+class _CommonFutureBuilderState extends State<CommonFutureBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      builder: _buildFuture,
+      future: widget.future,
+    );
+  }
+
+  Widget _buildFuture(BuildContext context, AsyncSnapshot snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+        return Center(
+          child: Text('Not currently connected to any asynchronous computation',
+              style: TextStyle(color: Colors.transparent)),
+        );
+      case ConnectionState.active:
+        return Center(
+          child: Text('ConnectionState.active',
+              style: TextStyle(color: Colors.transparent)),
+        );
+      case ConnectionState.waiting:
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      case ConnectionState.done:
+        if (snapshot.hasError) {
+          if (widget.connectionError != null) {
+            widget.connectionError(snapshot.error);
+          }
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+        if (widget.connectionSucceed != null) {
+          widget.connectionSucceed(snapshot.data);
+        }
+        return widget.contentWidget != null
+            ? widget.contentWidget
+            : Container(
+                child: Text('data: ${snapshot.data.toString()}'),
+              );
+      default:
+        return null;
+    }
+  }
+}
