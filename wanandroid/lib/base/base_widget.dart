f70db8829fef0ecff902a073f447374d81a6c4c9
androidlgf <android_lgf@163.com>
2019-04-04 11:24:41 +0800

    add loadWidget

--- wanandroid/lib/base/base_widget.dart
+++ wanandroid/lib/base/base_widget.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:wanandroid/widget/load_dialog.dart';
 import 'package:wanandroid/config/component_index.dart';
 
 abstract class BaseStatelessWidget extends StatelessWidget {
+  static BuildContext _loadContext;
   @override
   Widget build(BuildContext context) {
+    _loadContext=context;
     return null;
   }
 
-  void showLoading() {}
+  void showLoading() {
+    showDialog(
+        context: _loadContext,
+        builder: (BuildContext context) {
+          return LoadDialog();
+        });
+  }
 
-  void hideLoading() {}
+  void hideLoading() {
+    Navigator.pop(_loadContext);
+  }
 
   //Toast提示/
   void showMessage(String message) {
@@ -29,5 +40,17 @@ abstract class BaseStatelessWidget extends StatelessWidget {
   //关闭键盘/
   void hideKeyboard() {
     SystemChannels.textInput.invokeMethod('TextInput.hide');
+  }
+
+  Future<bool> isNetworkConnected() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      // I am connected to a mobile network.
+      return true;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      // I am connected to a wifi network.
+      return true;
+    }
+    return false;
   }
 }
--- wanandroid/lib/config/component_index.dart
+++ wanandroid/lib/config/component_index.dart
@@ -1,8 +1,10 @@
+export 'dart:io';
 export 'dart:convert';
 export 'package:rxdart/subjects.dart';
 export 'package:flutter/services.dart';
 export 'package:common_utils/common_utils.dart';
 export 'package:fluttertoast/fluttertoast.dart';
+export 'package:connectivity/connectivity.dart';
 export 'package:wanandroid/blocs/bloc_provider.dart';
 export 'package:pull_to_refresh/pull_to_refresh.dart';
 export 'package:cached_network_image/cached_network_image.dart';
